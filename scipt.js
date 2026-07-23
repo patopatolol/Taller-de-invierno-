22083d68da82a53e97a5ac7f03bd75f2e90f8e28
patopatolol <raisachacon81@gmail.com>
2026-07-23 19:46:52 -0400

    ruletaaaaaaaaaaaaaaaaaa

--- scipt.js
+++ scipt.js
@@ -1,12 +1,66 @@
-function mostrarMateriales() {
-    const caja = document.getElementById("materiales");
-
-    if (caja.classList.contains("oculto")) {
-        caja.classList.remove("oculto");
-        caja.classList.add("visible");
-    } else {
-        caja.classList.remove("visible");
-        caja.classList.add("oculto");
-        boton.innerHTML = "Ocultar materiales";
+let imagenes = [
+    {
+        "url": "../Fotos/qr.png",
+        "nombre": "Proyecto 01",
+        "descripcion": "comida"
+    },
+    {
+        "url": "../Fotos/habican.png",
+        "nombre": "Proyecto 02",
+        "descripcion": "dinero"
+    },
+    {
+        "url": "../Fotos/thor.jpg",
+        "nombre": "Proyecto 03",
+        "descripcion": "mucho dineromucho dineromucho dineromucho dineromucho dinero"
     }
+];
+
+let atras = document.getElementById('atras');
+let adelante = document.getElementById('adelante');
+let imagen = document.getElementById('img');
+let puntos = document.getElementById('puntos');
+let texto = document.getElementById('texto')
+let actual = 0
+posicionCarrusel()
+
+atras.addEventListener('click', function(){
+    actual -=1
+
+    if (actual == -1){
+        actual = imagenes.length - 1
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+adelante.addEventListener('click', function(){
+    actual +=1
+
+    if (actual == imagenes.length){
+        actual = 0
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+
+function posicionCarrusel() {
+    puntos.innerHTML = ""
+    for (var i = 0; i <imagenes.length; i++){
+        if(i == actual){
+            puntos.innerHTML += '<p class="bold">.</p>'
+        }
+        else{
+            puntos.innerHTML += '<p>.</p>'
+        }
+    } 
 }
